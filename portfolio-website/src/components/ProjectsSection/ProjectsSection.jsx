256701a7f6aa9a3c8ad645830fba426ec45dfd45
Daniel Narvaez <github.uabx3@passfwd.com>
2024-12-21 17:20:33 -0500

    Update ProjectsSection.jsx

--- portfolio-website/src/components/ProjectsSection/ProjectsSection.jsx
+++ portfolio-website/src/components/ProjectsSection/ProjectsSection.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, memo, useState, useRef, useEffect } from "react";
+import React, { useContext } from "react";
 
 import { GameProjectArticle } from "../GameProjectArticle/GameProjectArticle";
 
