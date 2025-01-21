38209a1d3eb2b891f24684973c5cd19bc71847e4
Daniel Narvaez <github.uabx3@passfwd.com>
2025-01-21 14:31:25 -0500

    new font

--- portfolio-website/src/main.jsx
+++ portfolio-website/src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+
 import App from './routes/App/App.jsx'
 
-import "@fontsource/baskervville-sc"
+import '@fontsource-variable/outfit';
+import '@fontsource/baskervville-sc';
 import '@fontsource/libre-baskerville';
-import "@fontsource-variable/noto-sans"
+import '@fontsource-variable/noto-sans';
 
 document.documentElement.style.setProperty('--cursor-style', import.meta.env.DEV ? 'auto' : 'none');
 
